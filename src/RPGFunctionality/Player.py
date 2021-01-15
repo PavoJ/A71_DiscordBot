2030755aaadc4fec8b0b6199340a6d5aff8c8282
PavoJ <xxnanni2002xx@gmail.com>
2021-01-15 14:47:09 +0100

    Implementata la funzionalità delle pagine dell'inventario

--- src/RPGFunctionality/Player.py
+++ src/RPGFunctionality/Player.py
@@ -1,10 +1,12 @@
 from DBTable import DBTable
 from commands.wrappers import aesthetic_discord
 
+
 class Player:
     def __init__(self, player_id):
         self._inventoryTable = DBTable('inventory')
         self.inventory = self._inventoryTable.getEntries(playerID=player_id)
+        self.pagelen = 10
 
         self._userTable = DBTable('player')
         self.userdata = self._userTable.getEntry(playerID=player_id)
@@ -14,7 +16,7 @@ class Player:
             self.userdata = self._userTable.getEntry(playerID=player_id)
 
     def addXP(self, qnt):
-        self._userTable.changeEntry("playerID", self.userdata.playerID, "xp", self.userdata.xp+qnt)
+        self._userTable.changeEntry("playerID", self.userdata.playerID, "xp", self.userdata.xp + qnt)
         self.userdata = self._userTable.getEntry(playerID=self.userdata.playerID)
 
     def addItem(self, item_id):
@@ -24,25 +26,38 @@ class Player:
     @aesthetic_discord.box
     def getItem(self, item_number) -> str:
         item_index = DBTable("items")
-        item = item_index.getEntry(ID=self.inventory[item_number-1].itemID)
+        item = item_index.getEntry(ID=self.inventory[item_number - 1].itemID)
 
         return f"{item.name} ({item.rarity}): \n{item.desc} "
 
+    @aesthetic_discord.box
     def getInvPage(self, page_number) -> str:
-        pass
+        items = DBTable("items")
+        retstr = ""
 
+        at_position = page_number * self.pagelen
+        for itemindex in range(at_position, at_position+self.pagelen):
+            if itemindex >= len(self.inventory):
+                break
+
+            itemrow = self.inventory[itemindex]
+            item = items.getEntry(ID=itemrow.itemID)
+
+            retstr = retstr + str(itemindex + 1) + ". " + \
+                    item.name + " (" + item.rarity + ")" + "\n"
+
+        return retstr
+
+    '''
     @aesthetic_discord.box
     def getInv(self) -> str:
-        item_index = DBTable("items")
+
         retstr = ""
         itemcnt = 1
-
-        for itemrow in self.inventory:
-            item = item_index.getEntry(ID=itemrow.itemID)
-            retstr = retstr + str(itemcnt) + ". " + item.name + " ("+item.rarity+")" + "\n"
-            itemcnt = itemcnt + 1
+        
+        retstr = getInvPage
 
         if retstr == "":
             return "non hai oggetti nel tuo inventario. è ora di grindare!"
 
-        return retstr
+        return retstr'''
--- src/commands/RPGCommands.py
+++ src/commands/RPGCommands.py
@@ -6,7 +6,7 @@ from RPGFunctionality.Player import Player
 
 class RPGCommands(commands.Cog):
     def __init__(self):
-        database = DBHandler()
+        self.invList = list()
 
     @commands.command(name="inventory", aliases=["inventario"],
                   help="visualizza e interagisci con il tuo inventario")
@@ -17,4 +17,40 @@ class RPGCommands(commands.Cog):
             if n_obj is not None:
                 await ctx.send(pRequester.getItem(n_obj))
             else:
-                await ctx.send(pRequester.getInv())
+                message = await ctx.send(pRequester.getInvPage(0))
+                await message.add_reaction('⬅')
+                await message.add_reaction('➡')
+
+                invreq = {"requester": {"user": ctx.author, "player": pRequester}, "message": message, "invPage": 0}
+
+                # to prevent memory leaks
+                if len(self.invList) > 100:
+                    self.invList = list()
+                self.invList.append(invreq)
+
+    # for scrolling through inventory pages
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, p):
+        eligible = False
+        request = None
+
+        for r in self.invList:
+            if p.message_id == r["message"].id and p.user_id == r["requester"]["user"].id:
+                request = r
+                pRequester = request["requester"]["player"]
+                eligible = True
+
+                if p.emoji.name == '⬅' and request["invPage"] > 0:
+                    request["invPage"] = request["invPage"]-1
+                elif p.emoji.name == '➡' and len(pRequester.inventory)/pRequester.pagelen > request["invPage"]+1:
+                    request["invPage"] = request["invPage"]+1
+                else:
+                    eligible = False
+
+                break
+
+        if eligible:
+            page = request["requester"]["player"].getInvPage(request["invPage"])
+            await request["message"].edit(content=page)
+
+        return
